8df27b96bab69e7843637f902f4c940ef963baa1
Sandra Coburn <sandra_coburn@hotmail.com>
2020-12-03 09:38:17 -0800

    Created an endpoint to return the visitors for a given date and an endpoint that returns visitors numbres in a range of two dates

--- data/migrations/20201202131515_create-tables.js
+++ data/migrations/20201202131515_create-tables.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable('visits', (tbl) => {
-    tbl.increments();
+    tbl.uuid('id').primary().defaultTo(knex.raw('(UUID())'));
     tbl.text('domain', 128).unique().notNullable();
     tbl.integer('visitors').unsigned().notNullable();
     tbl.text('date').notNullable();
