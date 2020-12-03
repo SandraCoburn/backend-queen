d177bae97c9f544313a360561cf02a74a6b5213c
Sandra Coburn <sandra_coburn@hotmail.com>
2020-12-03 06:48:05 -0800

    Created endpoint to get sites visited by a given date

--- domains/domains-router.js
+++ domains/domains-router.js
@@ -1,7 +1,7 @@
 const visitsRouter = require('./visits');
 const router = (app, fs) => {
-  app.get('./', (req, res) => {
-    res.send('Welcome to the development api-server');
+  app.get('/', (req, res) => {
+    res.send({ message: 'Welcome to the development api-server' });
   });
   visitsRouter(app, fs);
 };
--- domains/visits.js
+++ domains/visits.js
@@ -1,14 +1,58 @@
 const visitRoutes = (app, fs) => {
   const dataPath = './data/data.json';
 
-  //read
-  app.get('/visits', (req, res) => {
-    fs.readFile(dataPath, 'utf8', (err, data) => {
-      if (err) {
-        throw err;
+  //read from json file
+  app.get('/api/visits', async (req, res) => {
+    try {
+      fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+          throw Error('404');
+        }
+        res.status(200).send(JSON.parse(data));
+      });
+    } catch (err) {
+      if (err.toString() === 'Error: 404') {
+        return res.status(400).JSON({ message: 'Site not found' });
+      } else {
+        return res.status(500).JSON({
+          message: 'Something is wrong with the server. Try again later.',
+        });
       }
-      res.send(JSON.parse(data));
-    });
+    }
+  });
+  app.get('/api/visits/:date', async (req, res) => {
+    const data = require('../data/data.json');
+    try {
+      //Pull date out of URL parameter
+      const { date } = req.params;
+      console.log('hello', date);
+
+      const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      };
+      const filteredData = data.filter(
+        (site) => date == site.date.split('T')[0]
+      );
+      console.log(filteredData);
+      const sitesPerDay = filteredData.map((site) => {
+        return site.domain;
+      });
+      if (err) {
+        throw Error('404');
+      }
+
+      res.status(200).json({ date: sitesPerDay });
+    } catch (err) {
+      if (err.toString() === 'Error: 404') {
+        return res.status(400).JSON({ message: 'Site not found' });
+      } else {
+        return res.status(500).JSON({
+          message: 'Something is wrong with the server. Try again later.',
+        });
+      }
+    }
   });
 };
 module.exports = visitRoutes;
